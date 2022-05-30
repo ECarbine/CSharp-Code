c6d0891518ba70c84c6e7f62992c1eea49a9a97a
ECarbine <54371943+ECarbine@users.noreply.github.com>
2022-05-30 01:20:42 -0600

    Add files via upload

---
+++ Excercises/LabLinq1/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LabLinq1
+{
+    class Program
+    {
+        #region student list
+        private static List<Student> students = new List<Student> {
+            new Student("Don", "CS", 2015, true),
+            new Student("Dan", "CS", 2012, true),
+            new Student("Dee", "CS", 2013, false),
+            new Student("Bob", "CJ", 2013, false),
+            new Student("Ben", "CJ", 2013, true),
+            new Student("Jan", "FA", 2012, true),
+            new Student("Jim", "FA", 2014, false),
+            new Student("Rob", "EE", 2015, true),
+            new Student("Ray", "EE", 2012, true)
+         };
+        #endregion
+
+        #region Main
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nL I N Q   E X E R C I S E");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+            Console.WriteLine("\n\n1) Graduation years of FA students:");
+            ListGraudationYearOfFaStudents();
+
+            Console.WriteLine("\n\n2) Names and graduation years of CS students:");
+            ListNamesAndGraudationYearOfCsStudents();
+
+            Console.WriteLine("\n\n3) Names of CS students who graduate after 2012:");
+            ListNamesOfCsStudentsGraduatingAfter2012();
+
+            Console.WriteLine("\n\n4) All Graduation years without duplicates:");
+            ListGraduationYearsWithoutDuplicates();
+
+            Console.WriteLine("\n\n5) Graduation years withoug duplicates in descending order:");
+            ListGraduationYearsWithoutDuplicatesDescending();
+
+            Console.WriteLine("\n\n. . . press enter . . .");
+            Console.Read();
+        }
+        #endregion
+
+        // TODO 1:	Write a query that lists the graduation years of all fine art (FA) students 
+        //          then run the program to test the results
+        private static void ListGraudationYearOfFaStudents()
+        {
+            var majorYears =
+                from s1 in students
+                where s1.Major == "FA"
+                select s1.Year;
+
+            foreach(int x in majorYears)
+            {
+                Console.WriteLine(x);
+            }
+        }
+
+        // TODO 2:	Write a query that lists the names and graduation years of all computer science (CS) students
+        //          then run the program to test the results
+        private static void ListNamesAndGraudationYearOfCsStudents()
+        {
+            var csInfo =
+                from cs in students
+                where cs.Major == "CS"
+                select new { SName = cs.Name, gradYear = cs.Year };
+
+            foreach(var x in csInfo)
+            {
+                Console.WriteLine(x.SName + ": " + x.gradYear);
+            }
+        }
+
+        // TODO 3:	Write a query that lists the names of CS students that graduate after 2012 
+        //          then run the program to test the results 
+        private static void ListNamesOfCsStudentsGraduatingAfter2012()
+        {
+            var csGrads =
+                from cs in students
+                where cs.Major == "CS"
+                where cs.Year > 2012
+                select cs.Name;
+
+            foreach(var x in csGrads)
+            {
+                Console.WriteLine(x);
+            }
+        }
+
+        // TODO 4:	Write a query that lists all graduation years but no duplicates
+        //          then run the program to test the results 
+        private static void ListGraduationYearsWithoutDuplicates()
+        {
+            IEnumerable<int> noDups = (from s in students select s.Year).Distinct();
+
+            foreach(var year in noDups)
+            {
+                Console.WriteLine(year);
+            }
+
+        }
+
+        // TODO 5:	Write a query that lists all graduation years - but no duplicates - in descending order
+        //          then run the program to test the results 
+        private static void ListGraduationYearsWithoutDuplicatesDescending()
+        {
+            IEnumerable<int> noDups = (
+                from s in students 
+                select s.Year).Distinct().
+                OrderByDescending(x => x);
+
+
+            foreach (var year in noDups)
+            {
+                Console.WriteLine(year);
+            }
+        }
+    }
+}
---
+++ Excercises/LabLinq2/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabLinq2
+{
+    public class Program
+    {
+        #region student list
+        private static List<Student> students = new List<Student> {
+            new Student("Don", "CS", 2015, true),
+            new Student("Dan", "CS", 2012, true),
+            new Student("Dee", "CS", 2013, false),
+            new Student("Bob", "CJ", 2013, false),
+            new Student("Ben", "CJ", 2013, true),
+            new Student("Jan", "FA", 2012, true),
+            new Student("Jim", "FA", 2014, false),
+            new Student("Rob", "EE", 2015, true),
+            new Student("Ray", "EE", 2012, true)
+         };
+        #endregion
+
+        #region Main
+        static void Main(string[] args)
+        {
+            LinqChallenge2();
+        }
+
+        static void LinqChallenge2()
+        { 
+            Console.WriteLine("\nL I N Q   C H A L L E N G E 2:");
+
+            Console.WriteLine("\n\n======= Group students by honor (no group name):");
+            groupStudentsByHonorNoGroupName();
+
+            Console.WriteLine("\n\n======= Group students by honor (with group name):");
+            groupStudentsByHonorWithGroupName();
+
+            Console.WriteLine("\n\n======= Group students by major:");
+            groupStudentsByMajor();
+
+            Console.WriteLine("\n\n======= Group students by first letter of major:");
+            groupStudentsByFirstLetterOfMajor();
+
+            Console.WriteLine("\n\n======= Group student names by year:");
+            GroupStudentNamesByYear();
+
+            Console.WriteLine("\n\n======= Group student names by year (ordered by year):");
+            GroupStudentNamesByYearOrdered();
+
+            Console.WriteLine("\n\n======= Number of students per year:");
+            NumberOfStudentsPerYear();
+
+            Console.WriteLine("\n\n======= List Numbered Students:");
+            ListNumberedStudents();
+
+            Console.WriteLine("\n\n. . . press enter . . .");
+            Console.Read();
+        }
+        #endregion
+
+        #region groupStudentsByHonor()
+        private static void groupStudentsByHonorNoGroupName()
+        {
+
+        }
+
+        private static void groupStudentsByHonorWithGroupName()
+        {
+
+        }
+        #endregion
+
+        #region groupStudentsByMajor()
+        private static void groupStudentsByMajor()
+        {
+                
+        }
+        #endregion
+
+        #region groupStudentsByFirstLetterOfMajor()
+        private static void groupStudentsByFirstLetterOfMajor()
+        {
+
+        }
+        #endregion
+
+        #region GroupStudentNamesByYear
+        private static void GroupStudentNamesByYear()
+        {
+
+        }
+        #endregion
+
+        #region GroupStudentNamesByYearOrdered
+        private static void GroupStudentNamesByYearOrdered()
+        {
+
+        }
+        #endregion
+
+        #region NumberOfStudentsPerYear
+        private static void NumberOfStudentsPerYear()
+        {
+
+        }
+        #endregion
+
+        #region ListNumberedStudents
+        private static void ListNumberedStudents()
+        {
+
+        }
+        #endregion
+    }
+}
